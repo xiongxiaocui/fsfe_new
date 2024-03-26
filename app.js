a528e4bb0db27c62eba04e2f911ed5ef3a2774c0
xiongxiaocui <xiongxiaocui@sina.cn>
2024-03-26 03:14:07 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+http.createServer(function(req, res){
+  res.write('on the way to being a full stack engi');
+  res.end();
+}).listen(3000);
+console.log('server started on port 3000');
